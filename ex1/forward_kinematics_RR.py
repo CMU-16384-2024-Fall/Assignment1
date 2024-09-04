e8b0c4907382f0f3d0125bff918dc07394c736e8
shahram95 <shahramnajam.neduet@gmail.com>
2024-09-03 20:28:50 -0400

    Updated comments for ex1 (coding)

--- ex1/forward_kinematics_RR.py
+++ ex1/forward_kinematics_RR.py
@@ -7,13 +7,13 @@ def forward_kinematics_RR(theta1, theta2):
     l1 = 0.316  # length of the first link
     l2 = 0.384+0.088  # length of the second link 
 
-    # To-Do 1: Compute the homogeneous transformation matrices H_1_0, H_2_1, and H_2_0
+    # To-Do 1: Compute the homogeneous transformation matrices H_1_0, H_2_1, H_3_2, and H_4_3
     H_1_0 = np.eye(3)    
     H_2_1 = np.eye(3)
     H_3_2 = np.eye(3)
     H_4_3 = np.eye(3)
     
-    # To-Do 2: Compute the homogeneous transformation matrix H_2_0
+    # To-Do 2: Compute the homogeneous transformation matrix H_4_0
     H_4_0 = ...
     
     return {
